13592609b9914f96580eac44871e12ca81a663b5
Галсанпунцаг Бат-Орших <LP230092@G301-CC0176.local>
2023-11-30 16:02:42 +0800

    check login signup

--- Back_end/config/pgDb.js
+++ Back_end/config/pgDb.js
@@ -6,7 +6,7 @@ let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 const sql = postgres({
   host: PGHOST,
   database: PGDATABASE,
-  username: PGUSER,
+  userame: PGUSER,
   password: PGPASSWORD,
   port: 5432,
   ssl: "require",
