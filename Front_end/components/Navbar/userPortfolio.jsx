ee1570c86d1be295bf921e76b573f10fba5b7286
Галсанпунцаг Бат-Орших <LP230092@G301-CC0176.local>
2023-12-09 18:06:04 +0800

    user Info

--- Front_end/components/Navbar/userPortfolio.jsx
+++ Front_end/components/Navbar/userPortfolio.jsx
@@ -3,6 +3,8 @@ import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 
+import { FaPencilAlt } from "react-icons/fa";
+
 const style = {
   position: "absolute",
   top: "50%",
@@ -28,7 +30,12 @@ function ChildModal() {
 
   return (
     <React.Fragment>
-      <Button onClick={handleOpen}>Open Child Modal</Button>
+      <Button
+        onClick={handleOpen}
+        className="bg-primary hover:bg-blue-500 text-slate-100"
+      >
+        Choose Modal
+      </Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -40,7 +47,7 @@ function ChildModal() {
           <p id="child-modal-description">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
           </p>
-          <Button onClick={handleClose}>Close Child Modal</Button>
+          <Button onClick={handleClose}>Choose Modal</Button>
         </Box>
       </Modal>
     </React.Fragment>
@@ -58,18 +65,61 @@ export default function NestedModal() {
 
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
+      <Button onClick={handleOpen}>Profail</Button>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
-        <Box sx={{ ...style, width: 400 }}>
-          <h2 id="parent-modal-title">Text in a modal</h2>
-          <p id="parent-modal-description">
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </p>
+        <Box sx={{ ...style, width: 1000 }}>
+          <div className="flex">
+            <div className="flex flex-col">
+              <div className="avatar">
+                <div className="w-24 ml-10 mt-5 mb-5 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+              </div>
+              <button className="flex gap-4 mt-2 mb-5 bg-primary w-[200px] rounded-xl hover:bg-blue-700">
+                <p className="mt-1 ml-4">
+                  <FaPencilAlt />
+                </p>
+
+                <button className="">Edit Profile Picture</button>
+              </button>
+            </div>
+            <div className="">
+              <h className="text-2xl font-bold ">My Profile</h>
+              <div className="flex">
+                <div className="flex flex-col">
+                  <h>First Name</h>
+                  <input
+                    type="text"
+                    placeholder="Your first name"
+                    className="border-slate-500 border rounded-sm pl-2"
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <h>Last Name</h>
+                  <input
+                    type="text"
+                    placeholder="Your last name"
+                    className="border-slate-500 border rounded-sm pl-2"
+                  ></input>
+                </div>
+              </div>
+              <div className="flex">
+                <div className="flex flex-col">
+                  <h>First Name</h>
+                  <input
+                    type="text"
+                    placeholder="Your first name"
+                    className="border-slate-500 border rounded-sm pl-2"
+                  ></input>
+                </div>
+              </div>
+            </div>
+          </div>
           <ChildModal />
         </Box>
       </Modal>
